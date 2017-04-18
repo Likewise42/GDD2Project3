d320ce54fcf2500f262464292a6928ed7a61ea3c
Jesse Cooper <JesseAndCooper@Gmail.com>
2017-04-18 19:34:32 -0400

    Added despawning

--- Assets/Scripts/LevelSpawner.cs
+++ Assets/Scripts/LevelSpawner.cs
@@ -16,31 +16,22 @@ public class LevelSpawner : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         obstacles = new List<GameObject>();
-        spawnTimer = SPAWN_INTERVAL;
+        spawnTimer = SPAWN_INTERVAL / 2;
         startPos = new Vector3(-12.0f, 0.55f, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log(spawnTimer);
-        Debug.Log(obstacles.Count);
+        CullList();
 
         foreach (GameObject obst in obstacles)
         {
-            Vector3 obstPos = obst.transform.position;
-            obstPos.x += 0.1f;
-            obst.transform.position = obstPos;
+            MoveObject(obst);
         }
 
         if (spawnTimer <= 0)
         {
-            GameObject newObstacle = Instantiate(obstaclePrefab, obstaclePrefab.transform.position, Quaternion.identity);
-            Vector3 obstacleStartPos = startPos;
-            obstacleStartPos.z += Random.Range(-1.0f, 1.0f) * LEVEL_WIDTH;  // Randomize obstacle's starting z position
-
-            newObstacle.transform.position = obstacleStartPos;
-
-            obstacles.Add(newObstacle);
+            obstacles.Add(CreateObstacle());
 
             spawnTimer = SPAWN_INTERVAL;
         }
@@ -48,5 +39,53 @@ public class LevelSpawner : MonoBehaviour {
         {
             spawnTimer--;
         }
+    }
+
+    /// <summary>
+    /// CullList iterates over the obstacles list and removes any member for whom reachedEnd == true
+    /// </summary>
+    void CullList()
+    {
+        for (int i = obstacles.Count - 1; i >= 0; i--)
+        {
+            GameObject obst = obstacles[i];
+            Obstacle obstScript = obst.GetComponent<Obstacle>();
+            if (obstScript.ReachedEnd)
+            {
+                obstacles.RemoveAt(i);
+                Destroy(obst);
+            }
+        }
+    }
+
+    /// <summary>
+    /// MoveObject is a helper function to move a given GameObject in the level.
+    ///  Movement amounts are hard-coded in the function appropriate for the level.
+    /// </summary>
+    /// <param name="obst">The GameObject to move</param>
+    void MoveObject(GameObject obst)
+    {
+        Vector3 obstPos = obst.transform.position;
+        obstPos.x += 0.1f;
+        obst.transform.position = obstPos;
+    }
+
+    /// <summary>
+    /// CreateObstacle creates an instance of the obstacle prefab defined in the spawner
+    /// </summary>
+    /// <returns>A new obstacle GameObject</returns>
+    GameObject CreateObstacle()
+    {
+        GameObject newObstacle = Instantiate(obstaclePrefab,
+                                                obstaclePrefab.transform.position,
+                                                Quaternion.identity);
+
+        // Randomize obstacle's starting z position
+        Vector3 obstacleStartPos = startPos;
+        obstacleStartPos.z += Random.Range(-1.0f, 1.0f) * LEVEL_WIDTH;  
+
+        newObstacle.transform.position = obstacleStartPos;
+
+        return newObstacle;
     }
 }
