d8ae869ef8aca37dbb06af0950ff4dae94c44674
Ryan Muskopf <ryanjmuskopf@gmail.com>
2017-04-19 03:09:31 -0400

    multiple cameras now in store. toggle throught them with 1,2,3,4 on the keypad

---
+++ Assets/Scripts/storeScripts/storeGMScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class storeGMScript : MonoBehaviour
+{
+
+    public Camera mainCamera;
+    public Camera leftCamera;
+    public Camera rightCamera;
+    public Camera clerkCamera;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        mainCamera.enabled = true;
+        leftCamera.enabled = false;
+        rightCamera.enabled = false;
+        clerkCamera.enabled = false;
+
+    }
+
+    void disableAllCameras()
+    {
+        mainCamera.enabled = false;
+        leftCamera.enabled = false;
+        rightCamera.enabled = false;
+        clerkCamera.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            disableAllCameras();
+            mainCamera.enabled = true;
+        } else if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            disableAllCameras();
+            clerkCamera.enabled = true;
+        } else if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            disableAllCameras();
+            leftCamera.enabled = true;
+        } else if (Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            disableAllCameras();
+            rightCamera.enabled = true;
+        }
+    }
+}
