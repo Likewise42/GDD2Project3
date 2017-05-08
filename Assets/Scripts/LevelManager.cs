7d40a9f39462a87bd586adc21b995d62a1c5b9d6
Jesse Cooper <JesseAndCooper@Gmail.com>
2017-05-08 17:52:55 -0400

    Merging in pickups with stuff in master

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,286 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Level Manager contains logic for when to spawn objects/start events and holds level state
+/// </summary>
+public class LevelManager : MonoBehaviour {
+
+    public const int OBSTACLE_SPAWN_INTERVAL = 120;
+    public const int RAMP_SPAWN_INTERVAL = 120;
+    public const int CASH_SPAWN_INTERVAL = 80;
+    public const int PICKUP_SPAWN_INTERVAL = 140;
+    public const int TIME_TO_SLALOM = 600;
+    public const int LEVEL_END_SPAWN_INTERVAL = 3600;   // 1 minute level
+
+    // Slalom-specific constants
+    public const int NUMBER_OF_SLALOMS = 10;
+    private const int SLALOM_CHECKPOINT_TIMER = 10;
+    public const int TIME_BETWEEN_SLALOMS = 120;
+
+    public const float SLALOM_MULT_DELTA = .05f;
+
+    public uint slalomBasePoints = 100;
+    private uint gameEndTimer;
+    private int obstacleSpawnTimer;
+    private int rampSpawnTimer;
+    private int cashSpawnTimer;
+    private int pickupSpawnTimer;
+    private int levelEndSpawnTimer;
+    private int slalomTimer;
+
+    public bool hitSlalom;
+    private bool canSpawnSlalomCheckpoint;
+    private bool levelRunning = true;
+
+    private float timeInSeconds = 0.0f;
+
+    public uint coldCashScore = 100;
+
+    private uint currentColdCash;
+    private uint score;
+    private float slalomMultiplier = 0;
+    public float scoreMultiplier = 1;
+    private bool stopSpawning;
+    private bool slalomEvent;
+    private int currentSlalomCheckpointCount;
+
+    public GameScreenScript gameScreen;
+    public GameObject SpawnerObj;
+    private LevelSpawner spawner;
+
+    // Use this for initialization
+    void Start () {
+        // Start timer a little bit along
+        obstacleSpawnTimer = OBSTACLE_SPAWN_INTERVAL / 2;
+        rampSpawnTimer = RAMP_SPAWN_INTERVAL / 2 - 10;
+        cashSpawnTimer = 0;
+        levelEndSpawnTimer = 0;
+        slalomTimer = 0;
+        currentSlalomCheckpointCount = 0;
+
+        slalomEvent = false;
+        stopSpawning = false;
+
+        spawner = SpawnerObj.GetComponent<LevelSpawner>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (!levelRunning)
+        {
+            return;
+        }
+
+        timeInSeconds += Time.deltaTime;
+        if (slalomEvent)
+        {
+            //  --  Spawning    --
+            if (currentSlalomCheckpointCount <= NUMBER_OF_SLALOMS)
+            {
+                if (slalomTimer == 0)
+                {
+                    spawner.CreateSlalomFlag();
+                    canSpawnSlalomCheckpoint = true;
+                    slalomTimer++;
+                }
+                else if (slalomTimer >= TIME_BETWEEN_SLALOMS)
+                {
+                    slalomTimer = 0;
+                }
+                else if (slalomTimer >= SLALOM_CHECKPOINT_TIMER && canSpawnSlalomCheckpoint)
+                {
+                    spawner.CreateSlalomCheckpoint();
+                    canSpawnSlalomCheckpoint = false;   // So we only spawn 1 checkpoint per slalom
+                    currentSlalomCheckpointCount++;
+                }
+                else
+                {
+                    slalomTimer++;
+                }
+            }
+            else
+            {
+                slalomEvent = false;
+            }
+        }
+        else if (!stopSpawning)
+        {
+            if (ShouldSpawnLevelEnd())
+            {
+                spawner.CreateLevelEnd();
+                stopSpawning = true;
+                return; // Don't bother running the rest of Update
+            }
+
+            if (ShouldStartSlalom())
+            {
+                slalomEvent = true;
+                canSpawnSlalomCheckpoint = false;
+                slalomTimer = -1;
+            }
+
+            if (ShouldSpawnObstacle())
+            {
+                spawner.CreateObstacle();
+            }
+
+            if (ShouldSpawnRamp())
+            {
+                spawner.CreateRamp();
+            }
+
+            if (ShouldSpawnCash())
+            {
+                spawner.CreateColdCash();
+            }
+
+            if (ShouldSpawnPickup())
+            {
+                // Randomly choose a pickup to spawn		
+                float decision = Random.Range(0, 1.0f);
+                if (decision < 0.33f)
+                {
+                    spawner.CreateBoostPickup();
+                }
+                else if (decision < 0.66f)
+                {
+                    spawner.CreateCashBonusPickup();
+                }
+                else
+                {
+                    spawner.CreateMultiplierPickup();
+                }
+            }
+
+            obstacleSpawnTimer++;
+            rampSpawnTimer++;
+            cashSpawnTimer++;
+            pickupSpawnTimer++;
+            levelEndSpawnTimer++;
+            slalomTimer++;
+
+            gameScreen.SetDistancePercent((float)levelEndSpawnTimer / (float)LEVEL_END_SPAWN_INTERVAL);
+        }
+	}
+
+    bool ShouldStartSlalom()
+    {
+        if (slalomTimer >= TIME_TO_SLALOM)
+        {
+            slalomTimer = 0;
+            slalomEvent = true;
+            hitSlalom = false;
+            return true;
+        }
+
+        return false;
+    }
+
+    bool ShouldSpawnObstacle()
+    {
+        if (obstacleSpawnTimer >= OBSTACLE_SPAWN_INTERVAL)
+        {
+            obstacleSpawnTimer = 0;
+            return true;
+        }
+
+        return false;
+    }
+
+    bool ShouldSpawnRamp()
+    {
+        if (rampSpawnTimer >= RAMP_SPAWN_INTERVAL)
+        {
+            rampSpawnTimer = 0;
+            return true;
+        }
+
+        return false;
+    }
+
+    bool ShouldSpawnCash()
+    {
+        if (cashSpawnTimer >= CASH_SPAWN_INTERVAL)
+        {
+            cashSpawnTimer = 0;
+            return true;
+        }
+
+        return false;
+    }
+
+    bool ShouldSpawnLevelEnd()
+    {
+        if (levelEndSpawnTimer >= LEVEL_END_SPAWN_INTERVAL)
+        {
+            levelEndSpawnTimer = 0;
+            return true;
+        }
+
+        return false;
+    }
+
+    bool ShouldSpawnPickup()
+    {
+        if (pickupSpawnTimer >= PICKUP_SPAWN_INTERVAL)
+        {
+            pickupSpawnTimer = 0;
+            return true;
+        }
+        return false;
+    }
+
+    public void EndLevel()
+    {
+        if (!levelRunning)
+        {
+            return;
+        }
+        levelRunning = false;
+        bool nhs = score > YetiGameData.HighScore;
+        gameScreen.EndGameUI((int)timeInSeconds, score, currentColdCash, nhs);
+        if (nhs)
+        {
+            YetiGameData.HighScore = score;
+        }
+
+    }
+
+    public void addScore(uint score)
+    {
+        this.score += (uint)(scoreMultiplier * score);
+        gameScreen.SetScore(this.score, scoreMultiplier);
+    }
+
+    public void addColdCash(uint coldCashAmount)
+    {
+        YetiGameData.ColdCash += (uint)YetiGameData.coldCashMultiplier;
+        currentColdCash += coldCashAmount;
+        addScore(coldCashScore);
+    }
+
+    public void procSlalom()
+    {
+        if (hitSlalom)
+        {
+            addScore(slalomBasePoints);
+            scoreMultiplier += SLALOM_MULT_DELTA;
+            slalomMultiplier += SLALOM_MULT_DELTA;
+            hitSlalom = false;
+        }
+        else
+        {
+            resetSlalomMult();
+        }
+    }
+
+    public void resetSlalomMult()
+    {
+        scoreMultiplier -= slalomMultiplier;
+        slalomMultiplier = 0;
+    }
+}
---
+++ Assets/Scripts/YetiMovement/Movement.cs
@@ -0,0 +1,294 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private Vector3 acceleration;
+    private Vector3 velocity;
+    private bool aDown;
+    private bool dDown;
+    private bool wDown;
+    private bool applyGrav;
+    private bool jump;
+
+    private float yStart;
+    private float xStart;
+    private float zStart;
+    public float boundaryLength;
+    public float sideSpeed;
+    public GameObject world;
+
+    private float rotation;
+
+    public LevelManager lManager;
+
+    // A little janky to have this in player, but. . .
+    public int coldCashMultiplier = 1;
+    public float coldCashBonusTimer = 0;
+    public const int COLD_CASH_BONUS_DURATION = 900; // 15 seconds
+
+    private float lastJumpForce;
+
+    private bool sideSpeedBoard;
+    private bool magnetSeekBoard;
+    private bool accelerationBoard;
+    private bool coinValueBoard;
+
+
+    private Snowboard sb;
+    private bool collidingWithRamp;
+    private bool collidingWithObstacle;
+    private bool justLeftRamp;
+    private bool resetRampJumping;
+    private bool finishedSpinningOut;
+    private bool lockMovement;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        yStart = transform.position.y;
+        xStart = transform.position.x;
+        zStart = transform.position.z;
+
+        collidingWithRamp = false;
+        justLeftRamp = false;
+        jump = false;
+        finishedSpinningOut = true;
+        lockMovement = false;
+
+        // Which board do I have?
+        sb = gameObject.GetComponentInChildren<Snowboard>();
+        sideSpeed = sb.sideSpeed;
+
+        sideSpeedBoard = sb.sideSpeedBoard; // not implemented
+        magnetSeekBoard = sb.magnetSeekBoard; // not implemented
+        accelerationBoard = sb.accelerationBoard; // not implemented
+        coinValueBoard = sb.coinValueBoard; // implemented
+
+        rotation = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        acceleration = Vector3.zero;
+
+        // d key
+        if (Input.GetKeyDown("d"))
+        {
+            Debug.Log("dDown");
+            Debug.Log("sideSpeed: " + sideSpeed);
+            acceleration += new Vector3(sideSpeed, 0, 0);
+            dDown = true;
+        }
+        if (Input.GetKeyUp("d"))
+        {
+            acceleration -= new Vector3(sideSpeed, 0, 0);
+            dDown = false;
+        }
+
+        // a key
+        if (Input.GetKeyDown("a"))
+        {
+            Debug.Log("aDown");
+            acceleration += new Vector3(-sideSpeed, 0, 0);
+            aDown = true;
+        }
+        if (Input.GetKeyUp("a"))
+        {
+            acceleration -= new Vector3(-sideSpeed, 0, 0);
+            aDown = false;
+        }
+
+
+        // jumping
+        if (transform.position.y > yStart) // if they are above the 'gound' apply gravity
+        {
+            applyGrav = true;
+        }
+        else // return the y velocity to 0
+        {
+            resetRampJumping = true;
+            applyGrav = false;
+            transform.position = new Vector3(transform.position.x, yStart, transform.position.z);
+            velocity.y = 0f;
+            acceleration.y = 0f;
+        }
+
+        // applies gravity
+        if (applyGrav)
+        {
+            acceleration.y -= 0.05f;
+        }
+        
+
+        // ramp collision resolution
+        if (collidingWithRamp)
+        {
+            Debug.Log("Colliding with ramp");
+            transform.Translate(new Vector3(0, 0.5f, 0));
+            jump = true;
+        }
+        else if(!collidingWithRamp && justLeftRamp && resetRampJumping)
+        {
+            print("just left ramp");
+            acceleration.y += world.GetComponent<WorldSpin>().speed / 9f + (transform.position.y - yStart) / 9f;
+            justLeftRamp = false;
+            jump = false;
+            resetRampJumping = false;
+        }
+
+        // obstacle slowing
+        if (!finishedSpinningOut)
+        {
+            lockMovement = true;
+            world.GetComponent<WorldSpin>().Slow();
+            if (rotation >= 680)
+            {
+                rotation = 0;
+                finishedSpinningOut = true;
+                lockMovement = false;
+            }
+            else
+            {
+                rotation += 11f;
+            }
+            gameObject.transform.Rotate(new Vector3(0, rotation * Time.deltaTime, 0), Space.Self);
+        }
+        else
+        {
+            gameObject.transform.eulerAngles = new Vector3(0, 90, 0);
+        }
+
+
+        // changes velocity
+        velocity += acceleration;
+
+        // to lock horizontal movement when you hit a rock (Do you guys have suggestions? It looks iffy to me.)
+        if (!lockMovement)
+        {
+            // update position every frame
+            transform.Translate((velocity * Time.deltaTime) * 15, Space.World);
+        }
+        else
+        {
+            Vector3 tempVerticleMovementVec = new Vector3(0, velocity.y, 0); // to keep the player moving vertically while locked horizontally
+            transform.Translate((tempVerticleMovementVec * Time.deltaTime) * 15, Space.World);
+        }
+
+
+
+        // left and right boundaries
+        if (transform.position.x > xStart + boundaryLength)
+        {
+            Vector3 overflowVec = new Vector3(-1 * (transform.position.x - (xStart + boundaryLength)), 0, 0);
+            transform.Translate(overflowVec, Space.World);
+        }
+        else if (transform.position.x < xStart - boundaryLength)
+        {
+            Vector3 overflowVec = new Vector3(-1 * (transform.position.x + (xStart + boundaryLength)), 0, 0);
+            transform.Translate(overflowVec, Space.World);
+        }
+
+        
+        gameObject.GetComponent<Animator>().SetBool("Jumping", jump);
+    }
+    
+    void ExitCollideWithObstacle()
+    {
+        Debug.Log("Exiting ramp");
+        collidingWithObstacle = false;
+    }
+    
+    void ExitCollideWithRamp()
+    {
+        Debug.Log("Exiting ramp");
+        collidingWithRamp = false;
+        justLeftRamp = true;
+    }
+
+    void CollideWithColdCash()
+    {
+        if(coinValueBoard)
+        {
+            lManager.addColdCash(2);
+        }
+        lManager.addColdCash(1);
+        if(!this.GetComponent<AudioSource>().isPlaying)
+        {
+            this.GetComponent<AudioSource>().Play();
+        }
+        else
+        {
+            this.GetComponent<AudioSource>().Stop();
+            this.GetComponent<AudioSource>().Play();
+        }
+    }
+
+    void CollideWithCashBonus()
+    {
+        coldCashMultiplier = 2;
+        coldCashBonusTimer = COLD_CASH_BONUS_DURATION;
+    }
+
+    void CollideWithLevelEnd()
+    {
+        lManager.EndLevel();
+        world.GetComponent<AudioSource>().Stop();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject otherObj = other.gameObject;
+        switch (otherObj.tag)
+        {
+            case "Obstacle":
+                finishedSpinningOut = false;
+                break;
+            case "Ramp":
+                collidingWithRamp = true;
+                break;
+            case "LevelEnd":
+                lManager.EndLevel();
+                break;
+            case "ColdCash":
+                CollideWithColdCash();
+                break;
+            case "SlalomFlags":
+                lManager.hitSlalom = true;
+                break;
+            case "SlalomCheckpoint":
+                lManager.procSlalom();
+                break;
+            case "Pickup_Boost":
+                // Boost logic goes here - remember to add some sort of glow(?) to the player to indicate they have a powerup
+                break;
+            case "Pickup_CashBonus":
+                CollideWithCashBonus();
+                break;
+            case "Pickup_Multiplier":
+                lManager.scoreMultiplier += 0.25f;
+                break;
+            default:
+                break;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        GameObject otherObj = other.gameObject;
+        if (otherObj.CompareTag("Ramp"))
+        {
+            ExitCollideWithRamp();
+        }
+        if (otherObj.CompareTag("Obstacle"))
+        {
+            ExitCollideWithObstacle();
+        }
+    }
+
+}
+ 
