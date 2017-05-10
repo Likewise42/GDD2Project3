7af568d43708090d1e91c8e1c8c0a64a251086ad
Jake Recoon <jrecoon@gmail.com>
2017-05-10 13:44:23 -0400

    Assets added to scene, Boards spin and bounce when rotating is set to true

---
+++ Assets/Scripts/storeScripts/RotateObjects.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObjects : MonoBehaviour {
+
+    public bool rotating;
+    private Vector3 startRot;
+    private float startY;
+
+	// Use this for initialization
+	void Start () {
+        rotating = false;
+        startRot = this.transform.eulerAngles;
+        startY = this.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (rotating)
+        {
+            this.transform.Rotate(new Vector3(0, 20 * Time.deltaTime, 0), Space.World);
+            this.transform.Translate(0, Mathf.Sin(Time.time * 2) * .005f, 0, Space.World);
+        }
+        else
+        {
+            this.transform.eulerAngles = startRot;
+            this.transform.position = new Vector3(this.transform.position.x, startY, this.transform.position.z);
+        }
+	}
+}
