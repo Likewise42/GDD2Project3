6cce4a23d135690de7ff245eecb8b8d2d9501c10
Jesse Cooper <JesseAndCooper@Gmail.com>
2017-05-14 23:48:41 -0400

    Added level end with new ramp

---
+++ Assets/Scripts/LevelEnd.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEnd : MonoBehaviour
+{
+    public GameObject LM;
+    private LevelManager lManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        LM = GameObject.FindGameObjectWithTag("LevelManager");
+        lManager = LM.GetComponent<LevelManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject otherObj = other.gameObject;
+        if (otherObj.CompareTag("Despawner"))
+        {
+            lManager.EndLevel();
+        }
+    }
+}
