0de7dd8ca0fee689f756de4f9c9ccb600a1b3d37
Aidan Kaufman (RIT Student) <ak3012@ad.rit.edu>
2017-05-14 20:11:30 -0400

    boards mostly working

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -12,7 +12,7 @@ public class LevelManager : MonoBehaviour {
     public const float CASH_SPAWN_INTERVAL = 80;
     public const float PICKUP_SPAWN_INTERVAL = 480;
     public const float TIME_TO_SLALOM = 1100;
-    public const float LEVEL_END_SPAWN_INTERVAL = 4200;   // 1 minute level
+    public const float LEVEL_END_SPAWN_INTERVAL = 420;   // 1 minute level
 
     // Slalom-specific constants
     public const int NUMBER_OF_SLALOMS = 10;
@@ -65,8 +65,13 @@ public class LevelManager : MonoBehaviour {
     public GameObject SpawnerObj;
     private LevelSpawner spawner;
 
+    public GameObject OGYeti;
+    public GameObject LankyYeti;
+
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         // Start timer a little bit along
         obstacleSpawnTimer = OBSTACLE_SPAWN_INTERVAL / 2;
         rampSpawnTimer = RAMP_SPAWN_INTERVAL / 2 - 10;
@@ -83,6 +88,23 @@ public class LevelManager : MonoBehaviour {
         levelObj = GameObject.FindGameObjectWithTag("World");
         // QualitySettings.vSyncCount = 0;  // VSync must be disabled
         // Application.targetFrameRate = 15;
+
+        // I'm sorry Jesse
+        OGYeti.SetActive(false);
+        LankyYeti.SetActive(false);
+
+        switch (YetiGameData.SelectedYeti)
+        {
+            case YetiGameData.YetiType.NormalYeti:
+                OGYeti.SetActive(true);
+                break;
+            case YetiGameData.YetiType.LankyYeti:
+                LankyYeti.SetActive(true);
+                break;
+            default:
+                OGYeti.SetActive(true);
+                break;
+        }
     }
 	
 	// Update is called once per frame
